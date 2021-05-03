b4723017b645eabed244d2fb9cffe391a1c10101
talha-cm <javedtalha023@gmail.com>
2021-05-03 13:58:36 +0500

    Update script.js
    Added functionality to check email validation ,check length validation, and also password check validation.

--- project1/script.js
+++ project1/script.js
@@ -5,52 +5,74 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Function to update class and message for errors
-function showError(input, message) {
-    // Get the parent element of the input field (.form-control)
+// All Functions
+// Function to show error
+function showError(input,message) {
     const formControl = input.parentElement;
-    // Replace the class - add error
     formControl.className = 'form-control error';
-    // Get the small element for the error message
     const small = formControl.querySelector('small');
-    // Replace the text for small element using the input message
     small.innerText = message;
 }
 
-// Function to update class for success
+// Function to show success
 function showSuccess(input) {
-    // Get the parent element of the input field (.form-control)
     const formControl = input.parentElement;
-    // Replace the class - add success
     formControl.className = 'form-control success';
 }
+
 // Function to check if email is valid
-function isVslidEmail(email){
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}   
-// Function to check if required fields have data 
- function checkRequired(inputArray) {
-     inputArray.forEach(function(input){
-         if ( input.value === '' ) {
-             console.log(input.id);
-             showError(input,`${getFieldid(input)} is required`);
-         }
-         else{
-             showSuccess(input);
-         }
-     });
- }
-// Function to get the id of the input field with proper case
-function getFieldid(input){
-    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input, `This is not a valid email`)
+    }
 }
-// Event Listeners
-// Create event listener for submit button
-form.addEventListener('submit', function(e) {
-    // Stop page from reloading on submit
+
+// Function to check if required fields have data
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        if ( input.value === '' ) {
+            console.log(input.id);
+            showError(input,`${getFieldId(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Function to check length of input field
+function checkLength(input, min, max) {
+    if ( input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2) {
+    if ( input1.value !== input2.value ) {
+        showError(input2,"Passwords don't match")
+    }
+}
+
+// Function to get the id of the input field with proper case
+function getFieldId(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// This is an event listener for the form on submit
+form.addEventListener('submit',function(e) {
     e.preventDefault();
 
     checkRequired([username,email,password,password2]);
-    
+    checkLength(username,3,10);
+    checkLength(password,6,15);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
+
 })
